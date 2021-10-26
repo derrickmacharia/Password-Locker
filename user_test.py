91d62c3457ffbf663c0b80216acdaac17bb05e52
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-10-26 10:13:35 +0300

    Run the first test which was successful

--- user_test.py
+++ user_test.py
@@ -1,3 +1,32 @@
 import unittest # Importing the unittest module
 from user import User # Importing the user class
 
+class TestUser(unittest.TestCase):
+
+    '''
+
+    Test class that defines test cases for the user class behaviours
+
+    Args:
+        unittest.TestCase class that helps in creating test cases
+
+    '''
+    def setUp(self):
+
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Der","Rick","derrick@gm.com") #create user object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Der")
+        self.assertEqual(self.new_user.last_name,"Rick")
+        self.assertEqual(self.new_user.email,"derrick@gm.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
