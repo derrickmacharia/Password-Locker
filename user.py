a57aff52e2cc8a97c45e211f4681316bbbed5855
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-10-26 08:43:16 +0300

    declared user variables

--- user.py
+++ user.py
@@ -4,4 +4,10 @@ class User:
     Class that generates new users
     """
 
-    def __init__(self, username, password):
+    userlist = [] # Empty user list   
+
+    def __init__(self, first_name, last_name, email):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
---
+++ user_test.py
@@ -0,0 +1,3 @@
+import unittest # Importing the unittest module
+from user import User # Importing the user class
+
