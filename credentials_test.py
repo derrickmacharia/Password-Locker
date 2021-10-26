5836314167c138b7ac8381a1a82edbd5be8e7451
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-10-26 22:10:16 +0300

    add test init in credentials

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,17 @@
 import unittest
 from credentials import Credentials 
+
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        """
+        Setup for the test
+        """
+        self.new_credentials = Credentials("Twitter","tweetUser", "pass123")
+
+    def test_init(self):
+        """
+            test to check credentials object is created
+        """
+        self.assertEqual(self.new_credentials.account_platform, "Twitter")
+        self.assertEqual(self.new_credentials.account_username, "tweetUser")
+        self.assertEqual(self.new_credentials.account_password, "pass123")
